f8ff6145e5eea66db4cb0430af87186bd5dcd58d
Vinicius <dev.hyperioncube@gmail.com>
2021-05-18 00:24:54 -0300

    :tada: Desafio Finalizado!

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -15,15 +15,29 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+    const addTask ={
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldState => [...oldState, addTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskComplete = tasks.map(task => {
+      return task.id === id ? { ...task, isComplete: !task.isComplete} : task
+    });
+    setTasks(taskComplete);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const delTask = tasks.filter(task => {
+      return task.id !== id
+    });
+    setTasks(delTask);
+ 
   }
 
   return (
